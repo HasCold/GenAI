b7a124aac4126302a34dd3d8ec95783fe306a750
Hasan <ha03330224926@gmail.com>
2025-09-12 14:05:38 -0700

    add rag system

---
+++ RAG_System/LLM.js
@@ -0,0 +1,14 @@
+// We have set the 768 diemensions in vector DB (Pinecone). It means we searched based on semantics or relevancy.
+
+// Phase 1: PDF or Document Loading
+// Phase 2: Chunking
+// Phase 3: Convert each chunk into vector
+// Phase 4: Store the vectors in the vector DB (Pinecone)
+// Phase 5: Query the vector DB (Pinecone)
+// Phase 6: Retrieve the most semantic or relevancy chunks
+// Phase 7: Generate the response
+
+// So all these phases are done by the different various ways. So we can used the Langchain framework to do all these phases.
+// Langchain is a utility tool which has a pre-build functions, modules and tools which ease our work basically.
+
+
